65990bdb50d17781540f084830561266b2830cef
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-05-07 03:26:50 +0530

    project completed from the video

--- script.js
+++ script.js
@@ -23,12 +23,14 @@ xhr.onload = function () {
     articles.forEach(function (element) {
       let news = ` <div class="col">
                     <div class="card">
+                    <div class="card-header">${element.source['name']}</div>
                       <img src="${element["urlToImage"]}" class="card-img-top"
                         alt="" />
                       <div class="card-body">
                         <h5 class="card-title">${element["title"]}</h5>
                         <p class="card-text">${element["description"]}</p>
                       </div>
+                      <a href="${element['url']}" target="_blank" class="btn btn-primary">Click here to Read more</a>
                       <div class="card-footer">
                     <small class="text-body-secondary">${element["publishedAt"]}</small>
                       </div>
