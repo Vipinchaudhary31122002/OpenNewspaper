49b3becb0b8a3c68f4c4c02687e0edc219ce4b7b
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2023-10-24 17:52:35 +0530

    fix the problem and add the title below navbar now i have to work on the new container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+// importing all the components
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+
+// importing all the styles
+import "./App.css";
+import "./Styles/Navbar.css";
+import "./Styles/Hero.css"
 
 function App() {
   return (
     <>
-      <h1>NewsX</h1>
+    <Navbar></Navbar>
+    <Hero></Hero>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="Hero">
+        {/* title below the navbar */}
+        <h1>
+          <span class="badge bg-primary m-auto">Trending News</span>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
