eaebe99226abcf8ec1d60e793d827a4d1b449a37
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-01-13 12:08:49 +0530

    added code for integration of rapid api into project and now i just have to test it in production

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@
 // importing libraries
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 // importing component
 import NewsCard from "../NewsCard/NewsCard";
@@ -10,25 +11,43 @@ import Error from "../Error/Error";
 // importing stylesheet
 import "./Hero.css";
 
+// defining the method, url, headers and data paratmeter for api
+const options = {
+  method: "POST",
+  url: "https://newsnow.p.rapidapi.com/newsv2",
+  headers: {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": import.meta.env.API_KEY,
+    "X-RapidAPI-Host": "newsnow.p.rapidapi.com",
+  },
+  data: {
+    query: "AI",
+    page: 1,
+    time_bounded: true,
+    from_date: "01/02/2021",
+    to_date: "05/06/2021",
+    location: "",
+    category: "",
+    source: "",
+  },
+};
+
 // hero component function
 const Hero = (props) => {
   // usestate hook for managing the data which is fetched from the api.
   const [data, setdata] = useState("");
-
   // useeffect hook for calling the api so the data can be fetched
   useEffect(() => {
     const getapidata = async () => {
       props.setProgress(10);
       // try block set the connection to the api and get the response
       try {
-        const res = await fetch(
-          `https://newsdata.io/api/1/news?&apikey=${
-            import.meta.env.VITE_REACT_APP_NEWS_API
-          }&category=${props.category}&language=en&country=in&full_content=1`
-        );
+        const response = await axios.request(options);
+        console.log(response.data);
         props.setProgress(30);
-        // setting the responcse in the json formart in newsdata variable
-        const newsdata = await res.json();
+        // setting the response in the json formart in newsdata variable
+        const newsdata = await response.data;
+        // console.log(newsdata);
         props.setProgress(60);
         // updating the data by using setdata function
         setdata(newsdata);
@@ -58,14 +77,14 @@ const Hero = (props) => {
             <Error title="Rate Limit Exceeded Please Try Again Later" />
           ) : (
             // mapping all the results that are in the data state and calling the newscard component with title, image, url and source property passed as props.
-            Array.isArray(data?.results) &&
-            data?.results?.map((element, index) => (
+            Array.isArray(data?.items) &&
+            data?.items?.map((element, index) => (
               <div className="col" key={index}>
                 <NewsCard
                   title={element.title}
                   // if null value is returned in image in the json then news.png will displayed
-                  image={element.image_url ? element.image_url : "/news.png"}
-                  url={element.link}
+                  image={element.images ? element.image : "/news.png"}
+                  url={element.url}
                   source={element.source_id}
                 />
               </div>
