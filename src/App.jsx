1e87250cf99ba136485cc21d1e1243d8c3eb02ac
VipinChaudhary <vipinchaudhary31122002@gmail.com>
2024-02-13 23:01:49 +0530

    removed the react top loader bar insted i have put the circular loading bar in middle of which render before any request is made until data is fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 // importing libraries
-import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import LoadingBar from "react-top-loading-bar";
 
 // importing components
 import Homepage from "./pages/Homepage";
@@ -9,64 +7,25 @@ import Hero from "./Components/Hero/Hero";
 import Error from "./Components/Error/Error";
 
 // importing styleheet
-import "./App.css"
+import "./App.css";
 
 function App() {
-  // usestate hook for mainting the progress for loading bar
-  const [Progress, setProgress] = useState(0);
   return (
     <>
-      {/* loadingbar element */}
-      <LoadingBar
-        height={5}
-        progress={Progress}
-        onLoaderFinished={() => setProgress(0)}
-      />
-      {/* routing of all the pages */}
       <Routes>
-        {/* default route */}
         <Route exact path="/" element={<Homepage />}>
-          {/* default page to render on default route */}
-          <Route index element={<Hero setProgress={setProgress} />} />
-          {/* sports route */}
-          <Route
-            path="sports"
-            element={<Hero category="Sports" setProgress={setProgress} />}
-          />
-          {/* scicene route */}
-          <Route
-            path="science"
-            element={<Hero category="Science" setProgress={setProgress} />}
-          />
-          {/* business route */}
-          <Route
-            path="business"
-            element={<Hero category="Business" setProgress={setProgress} />}
-          />
-          {/* technology route */}
-          <Route
-            path="technology"
-            element={<Hero category="Technology" setProgress={setProgress} />}
-          />
-          {/* entertainment route */}
+          <Route index element={<Hero />} />
+          <Route path="sports" element={<Hero category="Sports" />} />
+          <Route path="science" element={<Hero category="Science" />} />
+          <Route path="business" element={<Hero category="Business" />} />
+          <Route path="technology" element={<Hero category="Technology" />} />
           <Route
             path="entertainment"
-            element={
-              <Hero category="Entertainment" setProgress={setProgress} />
-            }
+            element={<Hero category="Entertainment" />}
           />
-          {/* health route */}
-          <Route
-            path="health"
-            element={
-              <Hero category="Health" setProgress={setProgress} />
-            }
-          />
-          {/* error route if any error occurs */}
+          <Route path="health" element={<Hero category="Health" />} />
           <Route path="*" element={<Error />} />
         </Route>
-        {/* error route if any error occurs */}
-        <Route path="*" element={<Error />} />
       </Routes>
     </>
   );
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,98 +1,93 @@
-/* eslint-disable react/prop-types */
-// importing libraries
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
-
-// importing component
+import { CircularProgress } from "@mui/material";
 import NewsCard from "../NewsCard/NewsCard";
 import Error from "../Error/Error";
-
-// importing stylesheet
 import "./Hero.css";
 
-// hero component function
 const Hero = (props) => {
-  // usestate hook for managing the data which is fetched from the api.
-  const [data, setdata] = useState("");
-  // defining the method, url, headers and data paratmeter for api
+  const [data, setData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
   const options = {
-    method: 'GET',
-    url: 'https://news-api14.p.rapidapi.com/top-headlines',
+    method: "GET",
+    url: "https://news-api14.p.rapidapi.com/top-headlines",
     params: {
-      country: 'IN',
-      language: 'en',
-      pageSize: '10',
-      category: `${props.category}`
+      country: "IN",
+      language: "en",
+      pageSize: "10",
+      category: `${props.category}`,
     },
     headers: {
-      'X-RapidAPI-Key': `${import.meta.env.VITE_RAPIDAPI_KEY}`,
-      'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": `${import.meta.env.VITE_RAPIDAPI_KEY}`,
+      "X-RapidAPI-Host": "news-api14.p.rapidapi.com",
+    },
   };
-  // useeffect hook for calling the api so the data can be fetched
+
   useEffect(() => {
-    const getapidata = async () => {
-      props.setProgress(10);
-      // try block set the connection to the api and get the response
+    const fetchData = async () => {
+      setIsLoading(true); // Set loading state to true when fetching data
       try {
         const response = await axios.request(options);
-        props.setProgress(30);
         const newsdata = await response.data;
-        props.setProgress(60);
-        // updating the data by using setdata function
-        setdata(newsdata);
-        props.setProgress(100);
+        setData(newsdata);
       } catch (error) {
-        // catching if any error occurs
         console.log(error);
+      } finally {
+        setIsLoading(false); // Set loading state to false after data fetching is complete
       }
     };
-    // calling getapidata function using async arrow funciton
-    getapidata();
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.category]);
+
   return (
     <>
-      <div className="Hero">
-        {/* title below the navbar */}
-        <h1>
-          <span className="badge bg-primary m-auto">
-            Trending {props.category} News
-          </span>
-        </h1>
-        {/* newscard container which display all the news card */}
-        <div className="NewsCardContainer row row-cols-1 row-cols-md-4 g-4">
-          {/* if the result in the data is ratelimitexceeded the error component is called with rate limit exceeded please try again tomorrow  passed in the props with title property*/}
-          {data?.results?.code === "RateLimitExceeded" ? (
-            <Error title="Rate Limit Exceeded Please Try Again Later" />
-          ) : (
-            // mapping all the results that are in the data state and calling the newscard component with title, image, url and source property passed as props.
-            Array.isArray(data?.articles) &&
-            data?.articles?.map((element, index) => (
-              <div className="col" key={index}>
-                <NewsCard
-                  title={element.title}
-                  // if null value is returned in image and source in the json then news.png and OpenNewpaper title will displayed
-                  image={element.urlToImage ? element.urlToImage : "/news.png"}
-                  url={element.url}
-                  source={element.publisher.name ? element.publisher.name: "OpenNewspaper"}
-                />
-              </div>
-            ))
-          )}
+      {isLoading ? (
+        <>
+          <CircularProgress className="CircularLoader" />
+        </>
+      ) : (
+        <div className="Hero">
+          <h1>
+            <span className="badge bg-primary m-auto">
+              Trending {props.category} News
+            </span>
+          </h1>
+          <div className="NewsCardContainer row row-cols-1 row-cols-md-4 g-4">
+            {data?.results?.code === "RateLimitExceeded" ? (
+              <Error title="Rate Limit Exceeded Please Try Again Later" />
+            ) : (
+              Array.isArray(data?.articles) &&
+              data?.articles?.map((element, index) => (
+                <div className="col" key={index}>
+                  <NewsCard
+                    title={element.title}
+                    image={
+                      element.urlToImage ? element.urlToImage : "/news.png"
+                    }
+                    url={element.url}
+                    source={
+                      element.publisher.name
+                        ? element.publisher.name
+                        : "OpenNewspaper"
+                    }
+                  />
+                </div>
+              ))
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
 
-// default props
 Hero.defaultProps = {
   category: "All Categories",
 };
 
-// typechecking for props
 Hero.propTypes = {
   category: PropTypes.string,
 };
